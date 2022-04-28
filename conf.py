f02267931477ec44907c18deefdd667cbf308e8e
Nikita228rus <nikitafomin2001@mail.ru>
2022-04-28 13:45:17 +1000

    Finally

---
+++ conf.py
@@ -0,0 +1,135 @@
+import random
+import math
+
+
+def euclid_algorithm(a, b, flag):
+    r = [a, b]
+    x = [1, 0]
+    y = [0, 1]
+
+    i = 0
+    while r[i] != 0:
+        if r[i + 1] != 0:
+
+            q = (r[i] // r[i + 1])
+            c = r[i] - q * r[i + 1]
+            a = x[i] - q * x[i + 1]
+            b = y[i] - q * y[i + 1]
+
+            x.append(a)
+            y.append(b)
+            r.append(c)
+            i += 1
+
+        elif r[i + 1] == 0:
+            break
+
+    d = r[i]
+    u = x[i]
+    v = y[i]
+
+    if flag is True:
+
+        choose = input("1 - линейное представление НОД.\n2 - НОД.\n")
+        if choose == "1":
+            return f"{d} = {r[0]} * {u} + {r[1]} * {v}"
+        elif choose == "2":
+            return f"НОД{r[0], r[1]} = {d}"
+        elif choose != "1" and choose != "2":
+            return "Exit."
+
+    elif flag is False:
+        return [d, u, v]
+
+
+def test_miller2(n):
+    a = random.randint(1, n - 2)
+    exp = n - 1
+    while not exp & 1:
+        exp >>= 1
+
+    if pow(a, exp, n) == 1:
+        return True
+
+    while exp < n - 1:
+        if pow(a, exp, n) == n - 1:
+            return True
+        exp <<= 1
+
+    return False
+
+
+def generation_prime(k):
+    binary = []
+    for i in range(k):
+        bit = random.randint(0, 1)
+        binary.append(bit)
+
+    del binary[-1]
+    binary.append(1)
+    del binary[0]
+    binary.insert(0, 1)
+
+    p = int(''.join(str(x) for x in binary), 2)
+
+    test = []
+    for i in range(5):
+        test.append(test_miller2(p))
+
+    if test.count(True) == len(test):
+
+        return p
+
+    else:
+        return generation_prime(k)
+
+
+def func_rsa_generation(size):
+    p = generation_prime(size)
+    q = generation_prime(size)
+    n = q * p
+    func_euler = (p - 1) * (q - 1)
+    e = random.randint(3, func_euler - 1)
+
+    d = euclid_algorithm(e, func_euler, False)[1]
+    while euclid_algorithm(e, func_euler, False)[0] != 1:
+        e = random.randint(3, func_euler - 1)
+        d = euclid_algorithm(e, func_euler, False)[1]
+    while d < 0:
+        d += func_euler
+
+    coefficient = euclid_algorithm(q, p, False)[1]
+
+    return (n, e, d, p, q, pow(d, 1, p - 1), pow(d, 1, q - 1), coefficient)
+
+
+def int_to_bytes2(x: int) -> bytes:
+    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
+
+
+def int_to_bytes(x):
+    return x.to_bytes(math.ceil(math.log2(x) / 8), byteorder="big")
+
+
+def text_to_int(text: str) -> int:
+    return int.from_bytes(text.encode('utf-8'), 'big')
+
+
+def text_to_bin(text):
+    return ''.join(format(x, '08b') for x in bytearray(text, 'utf-8'))
+
+
+def bin_to_text(text):
+    temp_list = []
+    var = None
+    for i in range(len(text)):
+        if i % 8 == 0:
+            var = int(text[i:i + 8], 2)
+            var = hex(var)[2:].zfill(2)
+            temp_list.append(var)
+    result = b''
+    for i in temp_list:
+        print(i)
+        result += bytes.fromhex(i)
+    return result.decode()
+    # return bytes.fromhex(hex(int(text, 2))[2:]).decode()
---
+++ main.py
@@ -0,0 +1,126 @@
+from conf import *
+import random
+import json
+import timeit
+import sys
+
+
+# s = 'affffffffffafsfaaaaaaaaaaasfwagsgфафаhc♠♥♥☻♣○☻♦U♣♠7+♣♥♣+☺☺♂☺i♣affffffffffafsfaaaaaaaaaaasfwagsgфафаhc♠♥♥☻♣○☻♦U♣♠7+♣♥♣+☺☺♂☺i♣affffffffffafsfaaaaaaaaaaasfwagsgфафаhc♠♥♥☻♣○☻♦U♣♠7+♣♥♣+☺☺♂☺i♣affffffffffafsfaaaaaaaaaaasfwagsgфафаhc♠♥♥☻♣○☻♦U♣♠7+♣♥♣+☺☺♂☺i♣'
+
+
+def menu():
+    choose = input('1 - encryption\n2 - decryption\n3 - exit\n>>>\t')
+
+    if choose == '1':
+
+        choose = input('1 - read file\n2 - input keyboard\n>>>\t')
+        if choose == '1':
+            mess = open('input.txt', 'r', encoding='utf-8').read()
+        elif choose == '2':
+            mess = input('Input message:\t')
+
+        encrypt = rsa_encryption(mess)
+        open('output.txt', 'w', encoding='utf-8').write(encrypt)
+
+    elif choose == '2':
+
+        decrypt = rsa_decryption()
+        open('output.txt', 'w', encoding='utf-8').write(decrypt)
+
+    elif choose == '3':
+        exit()
+    menu()
+
+
+def rsa_decryption():
+    _message_struc_ = json.load(open('file_PKCS7.json'))
+    _private_key_ = json.load(open('file_PKCS12.json'))
+    _public_key_ = json.load(open('file_PKCS8.json'))
+    c = _message_struc_["EncryptedContentInfo"]["OPTIONAL"]
+    d = _private_key_['privateExponent']
+    n = _public_key_['SubjectPublicKeyInfo']['N']
+    m = [None] * len(c)
+    for i in range(len(c)):
+        m[i] = int_to_bytes(pow(c[i], d, n))
+
+    result = b''.join(m).decode('utf-8')
+    return result[:len(result) - _message_struc_['dump']]
+
+
+def rsa_encryption(message):
+    bytes_message = message.encode()
+
+    _data_ = {'dump': None,
+              'EncryptedContentInfo': {'ContentType': 'text',
+                                       'ContentEncryptionAlgorithmIdentifier': 'rsaEncryption',
+                                       'encryptedContent': None,
+                                       'OPTIONAL': None}}
+
+    _public_key_ = {'SubjectPublicKeyInfo': {
+        'publicExponent': None,
+        'N': None
+    }}
+
+    _private_key_ = {'privateExponent': None,
+                     'prime1': None,
+                     'prime2': None,
+                     'exponent1': None,
+                     'exponent2': None,
+                     'coefficient': None}
+
+    k = None
+    choose_k = input('size block:\n1 - 512\n2 - 1024\n3 - 2048\n>>>\t')
+    if choose_k == '1':
+        k = int(512 / 8)
+    elif choose_k == '2':
+        k = int(1024 / 8)
+    elif choose_k == '3':
+        k = int(2048 / 8)
+    else:
+        print('Try again...')
+        return rsa_encryption(message)
+
+    blocks_message = [bytes_message[x:x + k] for x in range(0, len(bytes_message), k)]
+
+    if len(blocks_message[-1]) != k:
+        l = len(blocks_message[-1])
+        temp_var = (k - l) % k
+        _data_['dump'] = temp_var
+        bytes_temp_var = int_to_bytes(temp_var)
+        for i in range(temp_var):
+            blocks_message[-1] = blocks_message[-1] + bytes_temp_var
+
+    key = func_rsa_generation(512)
+
+    _public_key_['SubjectPublicKeyInfo']['publicExponent'] = key[1]
+    _public_key_['SubjectPublicKeyInfo']['N'] = key[0]
+    _private_key_['privateExponent'] = key[2]
+    _private_key_['prime1'] = key[3]
+    _private_key_['prime2'] = key[4]
+    _private_key_['exponent1'] = key[5]
+    _private_key_['exponent2'] = key[6]
+    _private_key_['coefficient'] = key[7]
+
+    for i in range(len(blocks_message)):
+        blocks_message[i] = pow(int.from_bytes(blocks_message[i], 'big'), key[1], key[0])
+
+    _data_['EncryptedContentInfo']['OPTIONAL'] = blocks_message
+    encryptedContent = ''
+    for i in blocks_message:
+        encryptedContent = encryptedContent + hex(i)[2:]
+    _data_['EncryptedContentInfo']['encryptedContent'] = encryptedContent
+
+    json.dump(_data_, open('file_PKCS7.json', 'w+'))
+    json.dump(_public_key_, open('file_PKCS8.json', 'w+'))
+    json.dump(_private_key_, open('file_PKCS12.json', 'w+'))
+
+    return _data_['EncryptedContentInfo']['encryptedContent']
+
+
+menu()
+
+
+# func_rsa(s)
+# print(timeit.timeit('func()', number=10, globals=globals()))
+
+
